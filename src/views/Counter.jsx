b4f6e0aa2bc2255b1163e6ff7237d676d10d5b2e
Leo <hqyx45@qq.com>
2019-06-17 19:32:28 +0800

    :bug: 修复react编译不成功的问题

---
+++ src/views/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      count: ++this.state.count
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        当前count值：{this.state.count}
+        <br />
+        <button
+          style={{ border: '1px dashed blue' }}
+          onClick={() => this.handleClick()}
+        >
+          增加1
+        </button>
+      </div>
+    )
+  }
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import '../assets/home.scss'
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <h1 className="h-txt">This is Home Page!!!</h1>
+      </div>
+    )
+  }
+}
